dc799388030d0f2db273c203bc679fea85a13d0e
oumarou3 <omarouousseini2003@gmail.com>
2025-01-15 17:22:09 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "SampleLib.c"
+int main() {
+ int result = power( 2, 3 );
+ printf( "2³ == %d\n", result );
+ result = fact( 6 );
+ printf( "6! == %d\n", result );
+ return EXIT_SUCCESS;
+
+}
+
